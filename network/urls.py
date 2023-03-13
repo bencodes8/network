7e6e254d47405788fd80d5fac1f1e267b1b753dc
Ben Ku <benku12803@gmail.com>
2023-03-12 18:33:22 -1000

    added api path

--- network/urls.py
+++ network/urls.py
@@ -2,10 +2,12 @@
 from django.urls import path
 
 from . import views
+from .import api
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("api/posts", api.view_all_posts, name="view_all_posts")
 ]
