f3e5d69d01bdb59f8426bf8b2a97763594f7b903
Ben Ku <benku12803@gmail.com>
2023-03-19 22:16:20 -1000

    updated

--- network/urls.py
+++ network/urls.py
@@ -2,13 +2,11 @@
 from django.urls import path
 
 from . import views
-from .import api
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("api/posts", api.view_all_posts, name="view_all_posts"),
     path("profile/<int:user_id>", views.profile, name="profile"),
 ]
