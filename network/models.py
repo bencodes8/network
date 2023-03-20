74cb6f83d58f46044a27150042deb55defb4535b
Ben Ku <benku12803@gmail.com>
2023-03-19 19:06:37 -1000

    added followers, following, and liked posts fields for user

--- network/models.py
+++ network/models.py
@@ -3,4 +3,30 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    followers = models.IntegerField(default=0)
+    following = models.IntegerField(default=0)
+    liked_posts = models.ManyToManyField('Post', verbose_name='Liked Posts', related_name='user_liked_posts')
+    
+
+class Post(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    title = models.CharField(max_length=16, blank=False)
+    post = models.TextField()
+    likes = models.IntegerField(default=0)
+    date = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f'{self.user} posted {self.title} on {self.date}'
+    
+    def serialize(self):
+        return {
+            'id': self.id,
+            'user': self.user.username,
+            'user_id': self.user.pk,
+            'title': self.title,
+            'post': self.post,
+            'likes': self.likes,
+            'date': self.date.strftime('%Y-%m-%d')
+        }
+
