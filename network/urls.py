47c932fab8cc3d545fafbccf4297adaf2d32030a
Ben Ku <benku12803@gmail.com>
2023-03-19 19:05:56 -1000

    added profile path

--- network/urls.py
+++ network/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("api/posts", api.view_all_posts, name="view_all_posts")
+    path("api/posts", api.view_all_posts, name="view_all_posts"),
+    path("profile/<int:user_id>", views.profile, name="profile"),
 ]
